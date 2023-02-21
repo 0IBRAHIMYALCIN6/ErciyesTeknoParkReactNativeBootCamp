37ee08370d1c54e5a4d10b6a2aa2badcf3aa1545
İbrahim Yalçın <95691164+0IBRAHIMYALCIN6@users.noreply.github.com>
2023-02-21 18:24:56 +0300

    helloworld2

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import HelloWorld1 from './1-HelloWorld/HelloWorld1';
+import HelloWorld2 from './1-HelloWorld/HelloWorld2';
 
 export default function App() {
-  return <HelloWorld1/>
+  //return <HelloWorld1/>
+  return <HelloWorld2/>
 }
 
 
